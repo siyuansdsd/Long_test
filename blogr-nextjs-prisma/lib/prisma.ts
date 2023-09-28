a22ae1bd5e2f1635affd294b77b700a4748408eb
Douglas.Y <siyuan.yang2@uq.net.au>
2023-09-29 03:41:44 +1000

    again

--- blogr-nextjs-prisma/lib/prisma.ts
+++ blogr-nextjs-prisma/lib/prisma.ts
@@ -1,17 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+// 声明全局变量，使用PrismaClient或undefined类型
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma?: PrismaClient;
+    }
+  }
+}
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+// 创建Prisma实例，如果已经存在，则重复使用
+export const prisma = global.prisma || new PrismaClient({ log: ["query"] });
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// 在非生产环境下，将Prisma实例分配给全局变量
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
