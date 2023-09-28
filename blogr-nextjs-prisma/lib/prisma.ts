99db6fbdc996166b4817540eebfb35114eff56e7
Douglas.Y <siyuan.yang2@uq.net.au>
2023-09-29 04:09:13 +1000

    hhhh

--- blogr-nextjs-prisma/lib/prisma.ts
+++ blogr-nextjs-prisma/lib/prisma.ts
@@ -1,18 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-// 声明全局变量，使用PrismaClient或undefined类型
-declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma?: PrismaClient;
-    }
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
   }
+  prisma = global.prisma;
 }
 
-// 创建Prisma实例，如果已经存在，则重复使用
-export const prisma = global.prisma || new PrismaClient({ log: ["query"] });
-
-// 在非生产环境下，将Prisma实例分配给全局变量
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+export default prisma;
